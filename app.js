77e8189970e437401a558552aa1cc9963f42ae66
Vibrantlife <nicholesvibrantlife@gmail.com>
2015-06-05 01:59:37 -0700

    notes are fading out on delete

--- app.js
+++ app.js
@@ -1,31 +1,4 @@
-// add note
-function addNoteToList(note,id) {
-  $('#show-notes').prepend("<li><div class='edit' id='"+id+"'>"+note+"</div><a href='#' id='delete_"+id+"'>X</a></li>");
-  $("#"+id).editable("click",function(note,settings){
-      notes[id] = note.value;
-      localStorage['notes'] = JSON.stringify(notes);
-      return(note);
-    });
-
-  $('#delete_' + id).on('click', function () {
-    console.log(id);
-      $('#' + id).closest('li').remove();
-      notes.splice(id, 1);
-      localStorage['notes'] = JSON.stringify(notes);
-  });
-}
-
-if (localStorage['notes']) {
-  var notes = JSON.parse(localStorage[
-    'notes']);
-} else {
-  var notes = [];
-}
-
-for (var i=0; i < notes.length; i++) {
-  addNoteToList(notes[i],i);
-}
-
+// create note
 var addNote = function() {
   // get value from note input
   var noteValue = $('#note').val();
@@ -50,8 +23,47 @@ $('#note').keyup(function(event){
   }
 });
 
+if (localStorage['notes']) {
+  var notes = JSON.parse(localStorage[
+    'notes']);
+} else {
+  var notes = [];
+}
 
 
+
+//add note to list
+function addNoteToList(note,id) {
+  var $noteElement = $("<li><div class='edit' id='"+ id + "'>" + note +"</div><a href='#' id='delete_" + id + "'>X</a></li>");
+  var $form = $('#note-form');
+  var $noteList = $('#show-notes');
+  $newNote = $('#note');
+
+
+  // append note to div
+  $('#show-notes').prepend($noteElement).fadeIn('slow');
+
+// edit note
+  $("#"+id).editable("click",function(note,settings){
+      notes[id] = note.value;
+      localStorage['notes'] = JSON.stringify(notes);
+      return(note);
+    });
+// delete note
+  $('#delete_' + id).on('click', function () {
+      $(this).parent().fadeOut(1600, function() {
+        $(this).closest('li').remove();
+        console.log(notes);
+        notes.splice(id, 1);
+        localStorage['notes'] = JSON.stringify(notes);
+      });
+  });
+}
+
+// loop through all notes on list and add them
+for (var i=0; i < notes.length; i++) {
+  addNoteToList(notes[i],i);
+}
 
 
 
